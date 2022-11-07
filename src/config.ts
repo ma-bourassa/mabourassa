22a3c25a63f9cdfb9909189c6ce5594532abe3c6
Marc-Andre Bourassa <boumar04@gmail.com>
2022-11-07 11:42:40 -0500

    init

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 // Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// You can import this dat  a from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Modern Personal Website Template';
-export const SITE_DESCRIPTION =
-  'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE = {
+  title: 'Marc-André Bourassa Website',
+  description: 'Marc-André Bourassa personal website. Showcasing, projects, CV, blog, themes and responsive design.',
+  origin: 'https://mabourassa.dev',
+  basePathname: '/',
+};
