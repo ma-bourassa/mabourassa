a5f57d75c9fe16c655910702b36b0c9c72904a0d
Marc-Andre Bourassa <boumar04@gmail.com>
2022-11-08 21:52:37 -0500

    save

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,4 +1,9 @@
-const formatter = Intl.NumberFormat('fr-CA', { style: 'currency', currency: 'CAD' });
+const formatter = Intl.NumberFormat('fr-CA', {
+  style: 'currency',
+  currency: 'CAD',
+  currencyDisplay: 'narrowSymbol',
+  maximumFractionDigits: 0,
+});
 
 export function toDollars(units: number): string {
   return formatter.format(units / 100);
